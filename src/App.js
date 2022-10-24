d7ae5cf99959edae4622ba1cd537852cd969a01d
Bjørn Winther <bwinth18@student.aau.dk>
2022-10-24 23:27:29 +0200

    First milestone - Backup
    Works with reading Absolute sensor values. Prints correctly in console.debug on mobile browser chrome.
    
    Also added useState, that doesnt seem to update with fast refresh rate? but works somewhat.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,90 @@
 import logo from './logo.svg';
 import './App.css';
+import {Accelerometer, LinearAccelerationSensor, Sensor} from 'motion-sensors-polyfill'
+import {Gyroscope, AbsoluteOrientationSensor} from 'motion-sensors-polyfill'
+import { useState } from 'react';
 
-function App() {
+/*
+var xPos;
+var yPos;
+var zPos;
+var freqSensor;
+var varTest = 1;
+let letTest = "letTest"
+*/
+function App() {  
+
+  const options = { frequency: 60, referenceFrame: "device" };
+  const sensor = new AbsoluteOrientationSensor(options);
+  const [quaternion, setQuaternion] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+    w: 0
+  });
+
+  sensor.addEventListener("reading", () => {
+    // model is a Three.js object instantiated elsewhere.
+    //model.quaternion.fromArray(sensor.quaternion).inverse();
+    console.log(`Quart0 ${sensor.quaternion[0]}`);
+    console.log(`Quart1 ${sensor.quaternion[1]}`);
+    console.log(`Quart2 ${sensor.quaternion[2]}`);
+    console.log(`Quart3 ${sensor.quaternion[3]}`);
+    setQuaternion({x: sensor.quaternion[0],y: sensor.quaternion[1],z: sensor.quaternion[2],w: sensor.quaternion[3]});
+  });
+  
+  sensor.addEventListener("error", (error) => {
+    if (error.name === "NotReadableError") {
+      console.log("Sensor is not available.");
+    }
+  });
+  sensor.start();
+  //ask permissions
+  /*const sensor = new AbsoluteOrientationSensor();
+Promise.all([
+  navigator.permissions.query({ name: "accelerometer" }),
+  navigator.permissions.query({ name: "magnetometer" }),
+  navigator.permissions.query({ name: "gyroscope" }),
+]).then((results) => {
+  if (results.every((result) => result.state === "granted")) {
+    sensor.start();
+    // …
+  } else {
+    console.log("No permissions to use AbsoluteOrientationSensor.");
+  }
+});*/
+  /*
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+    z: 0
+  });*/
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <span>V8</span> 
+        <span>X: {quaternion.x}</span>
+        <span>Y: {quaternion.y}</span>
+        <span>Z: {quaternion.z}</span>
+        <span>W: {quaternion.w}</span>
+      </header>
+    </div>
+  );
+}
+
+/*
+<span>xPos:{xPos}</span>
+        <span>yPos:{yPos}</span>
+        <span>zPos:{zPos}</span>
+        <span>Frequency:{freqSensor}</span>
+        <span>varTest:{varTest}</span>
+        <span>letTest:{letTest}</span>
+*/
+//import { from } from 'list';
+
+//Was inside header
+/*<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +96,56 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
+*/
 
+//Code that works for accelerometer::
+/*
+if ('Accelerometer' in window) {
+    // The `Accelerometer` interface is supported by the browser.
+    // Does the device have an accelerometer, though?
+    console.log("Browser supports Accelerometer"); 
+    let accelerometer = null;
+    try {
+      accelerometer = new Accelerometer({ frequency: 10 }); // adjust frequency
+      //const gyroscope = new Gyroscope({ frequency: 15 });
+      //const orientation = new AbsoluteOrientationSensor({ frequency: 60 });
+      console.log("Accelerometer obj created");
+      accelerometer.onerror = (event) => {
+        console.log("onError function triggered");
+        // Handle runtime errors.
+        if (event.error.name === 'NotAllowedError') {
+          console.log('Permission to access sensor was denied.');
+        } else if (event.error.name === 'NotReadableError') {
+          console.log('Cannot connect to the sensor.');
+        }
+      };
+      accelerometer.onreading = (e) => {
+        //problem is reading?
+        //xPos = accelerometer.x;
+        //yPos = accelerometer.y;
+        //zPos = accelerometer.z;
+        //freqSensor = e.frequency;
+        console.log(e);
+      };
+      accelerometer.addEventListener('reading', () => {
+        console.log(`Acceleration along the X-axis ${accelerometer.x}`);
+        console.log(`Acceleration along the Y-axis ${accelerometer.y}`);
+        console.log(`Acceleration along the Z-axis ${accelerometer.z}`);
+        //xPos = accelerometer.x;
+        //yPos = accelerometer.y;
+        //zPos = accelerometer.z;
+      });
+      accelerometer.start();
+    } catch (error) {
+      // Handle construction errors.
+      console.log("fejl i catch");
+      if (error.name === 'SecurityError') {
+        console.log('Sensor construction was blocked by the Permissions Policy.');
+      } else if (error.name === 'ReferenceError') {
+        console.log('Sensor is not supported by the User Agent.');
+      } else {
+        throw error;
+      }
+    }
+  }*/
 export default App;
