841ec85f291a697696627cd9607df5a45b892082
Bjørn Winther <bwinth18@student.aau.dk>
2022-10-25 14:44:17 +0200

    Button & correct path implemented
    Button input, path and setting actioninput to default, including score upon launch under unique id.

--- src/App.js
+++ src/App.js
@@ -3,29 +3,45 @@ import './App.css';
 import {Accelerometer, LinearAccelerationSensor, Sensor} from 'motion-sensors-polyfill'
 import {Gyroscope, AbsoluteOrientationSensor} from 'motion-sensors-polyfill'
 import { useEffect, useState } from 'react';
-import { child, getDatabase, ref, set } from "firebase/database";
-import {database} from "./firebase"
+import { child, getDatabase, ref, set } from "firebase/database"; // Main Firebase implemenentation from general Lib
+import {database} from "./firebase" // referencing manually created firebase.js to reference database - like databaseRef.
 import { v4 as uuid } from 'uuid'; // Unique ID package for React
-//import { getDatabase, ref, set } from "firebase/database";
-//import {firebaseConfig, app} from "./firebase.js"
+import styled from 'styled-components' // lib for styling - `writing within these backticks is writing css.`
+import React from 'react';
 
+const Button = styled.button`
+  background-color: white;
+  padding: 70px 80px;
+  border-radius: 20px;
+  font-size: 20px;
+  font-family: monospace;
+  text-align: center;
+`
+const Span = styled.span`
+  padding: 60px;
+`
+const uniqueId = uuid(); // creating unique id with uuid lib - Replace with Ipv4 adress eventually if possible?
+
+function clickMe(){
+  sendShot(1,uniqueId);
+}
 
 function App() {  
 
-  //const options = { frequency: 60, referenceFrame: "device" };
-  //const sensor = new AbsoluteOrientationSensor(options);
-  const [quaternion, setQuaternion] = useState({
+  const [quaternion, setQuaternion] = useState({ // is currently not used. Was used to display Quart values on screen.
     x: 0,
     y: 0,
     z: 0,
     w: 0
   });
-  const uniqueId = uuid(); // creating unique id with uuid lib - Replace with Ipv4 adress eventually if possible?
+
+  //const uniqueId = uuid(); // creating unique id with uuid lib - Replace with Ipv4 adress eventually if possible?
   //const databaseRef = collection(database); // ??? is this how you declare it? - Think i can just use database now
 
   useEffect(() =>{ // Things are only called once because of []?
     const options = { frequency: 60, referenceFrame: "device" };
     const sensor = new AbsoluteOrientationSensor(options);
+    writeActionInput(0,0,uniqueId); // trigger once - to trigger actionInput in database and reset score
     sensor.addEventListener("reading", () => { //Callback function and overwrites the "only call once" - Updated every new reading
       //Console logs commented - works:
       //console.log(`Quart0 ${sensor.quaternion[0]}`);
@@ -50,7 +66,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <span>V9</span> 
+        <Span>V11</Span> 
+        <Button onClick={clickMe}>
+          Shoot
+        </Button>
       </header>
     </div>
   );
@@ -60,13 +79,28 @@ function App() {
 
   function writeSensorData(x, y, z, w, uniqueId) {
     const db = database;
-    set(ref(db, 'users/' + uniqueId + 'SensorInfo'), {
+    set(ref(db, 'users/' + uniqueId +'/'+ 'SensorInfo'), {
       xQuart: x,
       yQuart: y,
       zQuart : z,
       wQuart : w
     });
   }
+
+  function writeActionInput(input, resetScore, uniqueId) {
+    const db = database;
+    set(ref(db, 'users/' + uniqueId +'/'+ 'ActionInput'), {
+      score: resetScore,
+      shoot: input
+    });
+  }
+  function sendShot(input, uniqueId) {
+    const db = database;
+    set(ref(db, 'users/' + uniqueId +'/'+ 'ActionInput'), {
+      shoot: input
+    });
+  }
+
 export default App;
 
 //Commented code:
